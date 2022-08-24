331c8b6dbad6ae1ddfbb31253ae142ee3d1c6c8e
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-24 08:11:38 +0700

    change styling implementation due problem to scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './modules/landing/page/landing/landing.component';
+import { NavbarComponent } from './modules/landing/page/landing/navbar/navbar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingcardComponent } from './modules/landing/page/landing/landingcard/landingcard.component';
+import { LandingModule } from './modules/landing/landing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    LandingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './page/landing/navbar/navbar.component';
 import { LandingComponent } from './page/landing/landing.component';
+import { LandingcardComponent } from './page/landing/landingcard/landingcard.component';
 
 @NgModule({
   declarations: [
-    LandingComponent
+    NavbarComponent,
+    LandingComponent,
+    LandingcardComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    NavbarComponent,
+    LandingComponent,
+    LandingcardComponent
   ]
 })
 export class LandingModule { }

7c7a2c97374f926a386f71d573f9a6dec153cf53
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-24 19:49:07 +0700

    add more input in card

--- src/app/modules/landing/page/login/login/card/card.component.ts
+++ src/app/modules/landing/page/login/login/card/card.component.ts
@@ -9,27 +9,47 @@ import { FormControl, FormGroup, Validators} from '@angular/forms';
 export class CardComponent implements OnInit {
 
   formGroup: FormGroup;
+  emailColor: string = "primary";
+  passwordColor: string = "primary";
+
   constructor() {
     this.formGroup = new FormGroup({
       email: new FormControl("", {
         validators: [
           Validators.required, Validators.email
-        ]
+        ],
+        updateOn: "change"
+      }),
+      password: new FormControl("", {
+        validators: [
+          Validators.required, Validators.minLength(3)
+        ],
+        updateOn: "change"
       })
     });
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   emailValidation(): boolean{
     console.log("email validation terpanggil");
+    console.log(`email tidak valid = ${this.formGroup.controls["email"].invalid}`);
     if(this.formGroup.controls["email"].invalid){
+      this.emailColor = "warn";
       return false;
     } else {
+      this.emailColor = "accent";
       return true;
     }
   }
 
+  passwordValidation(): boolean{
+    if(this.formGroup.controls["password"].invalid){
+      this.passwordColor = "warn";
+      return false;
+    } else {
+      this.emailColor = "accent";
+      return true;
+    }
+  }
 }

b6e74c1fdf0b53bfa4a1d851a0c9d0a4e551726e
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-27 15:20:14 +0700

    change card styling in login page

--- src/app/modules/landing/page/register/register.module.ts
+++ src/app/modules/landing/page/register/register.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LandingModule } from '../landing/landing.module';
-
+import { CardComponent } from './register/card/card.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
-    LandingModule
+    LandingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ]
 })
 export class RegisterModule { }

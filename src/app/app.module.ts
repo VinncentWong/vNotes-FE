68db97822bac4ca2da24ff1777986c6b0ce9d320
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-08-28 11:25:37 +0700

    rechange module structure and change input appearence

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingModule } from './modules/landing/page/landing/landing.module';
 import { LoginModule } from './modules/landing/page/login/login.module';
 import { RegisterModule } from './modules/landing/page/register/register.module';
@@ -16,7 +15,7 @@ import { NotfoundModule } from './modules/landing/page/notfound/notfound.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
     LandingModule,
     LoginModule,
     RegisterModule,
--- src/app/modules/landing/page/notfound/notfound.module.ts
+++ src/app/modules/landing/page/notfound/notfound.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     NotfoundComponent
   ],
   imports: [
     CommonModule,
-    MatIconModule,
-    MatButtonModule
+    MatIconModule
+  ],
+  exports: [
+    NotfoundComponent
   ]
 })
 export class NotfoundModule { }
